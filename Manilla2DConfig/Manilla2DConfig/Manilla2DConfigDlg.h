3e45b3175a88a5e4bf927c712f3359934516b104
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-09-19 05:35:39 +0000

    fubar-ed the code... but it should be ready for a tab control now such as the one at http://www.codeguru.com/cpp/controls/controls/tabcontrols/article.php/c5239/

--- Manilla2DConfig/Manilla2DConfig/Manilla2DConfigDlg.h
+++ Manilla2DConfig/Manilla2DConfig/Manilla2DConfigDlg.h
@@ -3,7 +3,6 @@
 
 #pragma once
 
-#include "Manilla2DConfigUtils.h"
 
 // CManilla2DConfigDlg dialog
 class CManilla2DConfigDlg : public CDialog
@@ -29,34 +28,4 @@ protected:
 	afx_msg void OnSize(UINT /*nType*/, int /*cx*/, int /*cy*/);
 #endif
 	DECLARE_MESSAGE_MAP()
-public:
-    afx_msg void OnBnClickedMoveUpButton();
-    afx_msg void OnBnClickedMoveDownButton();
-    afx_msg void OnBnClickedCancelButton();
-    afx_msg void OnBnClickedRestoreDefaultsButton();
-    afx_msg void OnNMClickMainListControl(NMHDR *pNMHDR, LRESULT *pResult);
-    afx_msg void OnLvnItemchangedMainListControl(NMHDR *pNMHDR, LRESULT *pResult);
-
-private:
-	bool m_bPopulatingListControl;
-
-    NameAndEnabledState_vector_t m_newWidgetVector;
-    NameAndEnabledState_vector_t m_currentWidgetVector;
-
-    TodayScreenRegBackup m_todayScreenRegBackup;
-
-    CListCtrl m_mainListControl;
-    CButton m_moveUpButton;
-    CButton m_moveDownButton;
-    CButton m_cancelButton;
-    CButton m_restoreDefaultsButton;
-
-    int GetSelectedItemIndex();
-    void UpdateListControlFromNewWidgetVector();
-
-    void PopulateWidgetVectorsFromCurrentHTCHomeSettingsXmlFile();
-	void WriteHTCHomeSettingsXmlFileFromNewWidgetVector();
-	
-	void BackupTodayScreenItemsRegHive();
-	void RestoreTodayScreenItemsRegHive();
 };
