d1d975d36853cd1da76e466faaaa49cd10234c3c
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-09-20 04:32:02 +0000

    finally got the "Apply" "Cancel" soft keys working :)  WooHoo for stack overflow

--- trunk/Manilla2DConfig/Manilla2DConfig/Resourceppc.h
+++ trunk/Manilla2DConfig/Manilla2DConfig/Resourceppc.h
@@ -6,12 +6,9 @@
 #define IDD_MANILLA2DCONFIG_TABS_DIALOG 104
 #define IDD_MANILLA2DCONFIG_LAUNCHER_DIALOG 105
 #define IDR_MAINFRAME                   128
-#define IDR_MENU1                       129
-#define IDS_OK                          130
-#define IDS_CANCEL                      131
+#define IDR_OKCANCELMENU                129
 #define IDC_STATIC_1                    200
 #define IDC_MOVE_UP_BUTTON              1001
-#define IDC_CANCEL_BUTTON               1004
 #define IDC_MOVE_DOWN_BUTTON            1005
 #define IDC_RESTORE_DEFAULTS_BUTTON     1006
 #define IDC_MAIN_LIST_CONTROL           1007
@@ -21,7 +18,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
