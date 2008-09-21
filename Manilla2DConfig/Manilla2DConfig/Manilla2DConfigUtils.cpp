35b4ad81dbb1f1d2bafc7143ffbc289ee5d6056e
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-09-21 06:15:11 +0000

    added teh recursive delete function to the utils file, using the utils file in the setup dll now so that we can get the recursive directory delete and fileExists functionality from it

--- Manilla2DConfig/Manilla2DConfig/Manilla2DConfigUtils.cpp
+++ Manilla2DConfig/Manilla2DConfig/Manilla2DConfigUtils.cpp
@@ -498,3 +498,71 @@ void RestoreTodayScreenItemsRegHive()
         }
     }
 }
+
+void RecursivelyDeleteDirectory(CString sDirPath)
+{
+    // Declare variables
+    WIN32_FIND_DATA wfd;
+    HANDLE hFile;
+    DWORD dwFileAttr;
+    CString sFile;
+
+    CString sPathFile;
+
+    if(sDirPath[sDirPath.GetLength()-1] != '\\')
+    {
+        sDirPath += '\\';
+    }
+
+    CString sSpec = sDirPath;
+    sSpec += "*.*";
+
+    // Find the first file
+    hFile = FindFirstFile(sSpec, &wfd);
+
+    if(hFile != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            sFile = wfd.cFileName;
+            sPathFile = sDirPath + sFile;
+            // Get the file attributes
+            dwFileAttr = GetFileAttributes(sPathFile);
+
+            // See if file is read-only : if so unset read-only
+            if (dwFileAttr & FILE_ATTRIBUTE_READONLY)
+            {
+                dwFileAttr &= ~FILE_ATTRIBUTE_READONLY;
+                SetFileAttributes(sPathFile, dwFileAttr);
+            }
+
+            // See if the file is a directory
+            if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                // Make sure it isn't current or parent directory
+                if (sFile != "." && sFile != "..")
+                {
+                    sPathFile += "\\";
+
+                    // Recursively delete all files in this folder
+                    RecursivelyDeleteDirectory(sPathFile);
+
+                    // Remove the directory
+                    RemoveDirectory(sPathFile);
+                }
+            }
+            else
+            {
+                // Delete the file
+                DeleteFile(sPathFile);
+            }
+        }
+        while(FindNextFile(hFile, &wfd));
+    }
+
+    // Close handle to file
+    FindClose(hFile);
+
+    RemoveDirectory(sDirPath);
+}
+
