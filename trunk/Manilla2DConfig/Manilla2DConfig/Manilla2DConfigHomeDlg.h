bff70b48aad6f7a81ff2237607620e32d88e5efc
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-10-05 13:20:56 +0000

    added home dialog files to the project, updated the readme-themes with more details, updated the changelog

---
+++ trunk/Manilla2DConfig/Manilla2DConfig/Manilla2DConfigHomeDlg.h
@@ -0,0 +1,64 @@
+/*
+    This file is part of Manilla2DCustomizer.
+
+    Manilla2DCustomizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Manilla2DCustomizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Manilla2DCustomizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "Manilla2DConfigUtils.h"
+#include "afxwin.h"
+
+
+// CManilla2DConfigHomeDlg dialog
+
+class CManilla2DConfigHomeDlg : public CPropertyPage
+{
+	//DECLARE_DYNAMIC(CManilla2DConfigHomeDlg)
+
+public:
+	CManilla2DConfigHomeDlg(CWnd* pParent = NULL);
+	virtual ~CManilla2DConfigHomeDlg();
+
+// Dialog Data
+	enum { IDD = IDD_M2DC_HOME_DLG };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+    LRESULT OnQuerySiblings(WPARAM wParam, LPARAM lParam);
+
+    void OnPaint();
+
+public:
+    BOOL OnInitDialog();
+    void OnOK();
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+    CCommandBar m_cmdBar;
+
+    HomeWidgetSettings m_homeSettings;
+
+public:
+    CButton m_enableAnalogClockCheckBox;
+    CButton m_enableDigitalClockCheckBox;
+    CButton m_enableMissedCallsCheckBox;
+    CButton m_enableCalendarCheckBox;
+    CButton m_enableAlarmStatusCheckBox;
+    CButton m_enableLine1CheckBox;
+    CButton m_enableLine2CheckBox;
+    CButton m_enableLine3CheckBox;
+};
