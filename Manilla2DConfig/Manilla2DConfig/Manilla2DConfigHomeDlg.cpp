9375f1f681e15e31a70719a1383e8b8efa71fc84
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-10-05 13:20:56 +0000

    added home dialog files to the project, updated the readme-themes with more details, updated the changelog

---
+++ Manilla2DConfig/Manilla2DConfig/Manilla2DConfigHomeDlg.cpp
@@ -0,0 +1,152 @@
+/*
+    This file is part of Manilla2DCustomizer.
+
+    Manilla2DCustomizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Manilla2DCustomizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Manilla2DCustomizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Manilla2DConfigHomeDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Manilla2DConfig.h"
+#include "Manilla2DConfigHomeDlg.h"
+
+
+// CManilla2DConfigHomeDlg dialog
+
+//IMPLEMENT_DYNAMIC(CManilla2DConfigHomeDlg, CPropertyPage)
+
+CManilla2DConfigHomeDlg::CManilla2DConfigHomeDlg(CWnd* pParent /*=NULL*/)
+	: CPropertyPage(CManilla2DConfigHomeDlg::IDD, IDS_M2DC_HOME_STR)
+{
+
+}
+
+CManilla2DConfigHomeDlg::~CManilla2DConfigHomeDlg()
+{
+}
+
+void CManilla2DConfigHomeDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CPropertyPage::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_ANALOG_CB, m_enableAnalogClockCheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_DIGITAL_CB, m_enableDigitalClockCheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_MISSEDCALLS_CB, m_enableMissedCallsCheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_CALENDAR_CB, m_enableCalendarCheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_ALARM_CB, m_enableAlarmStatusCheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_LINE1_CB, m_enableLine1CheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_LINE2_CB, m_enableLine2CheckBox);
+    DDX_Control(pDX, IDC_M2DC_ENABLE_LINE3_CB, m_enableLine3CheckBox);
+}
+
+BOOL CManilla2DConfigHomeDlg::OnInitDialog()
+{
+    CPropertyPage::OnInitDialog();
+
+    m_cmdBar.Create(this);
+    m_cmdBar.InsertMenuBar(IDR_M2DC_APPLY_CANCEL_MENU);
+
+    M2DC::GetHomeWidgetSettings(M2DC::GetPathToHTCHomeSettingsXmlFileActual(), &m_homeSettings);
+
+    int check = m_homeSettings.bAnalogClockEnabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableAnalogClockCheckBox.SetCheck(check);
+
+    check = m_homeSettings.bDigitalClockEnabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableDigitalClockCheckBox.SetCheck(check);
+
+    check = m_homeSettings.bMissedCallsEnabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableMissedCallsCheckBox.SetCheck(check);
+
+    check = m_homeSettings.bCalendarEnabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableCalendarCheckBox.SetCheck(check);
+
+    check = m_homeSettings.bAlarmStatusEnabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableAlarmStatusCheckBox.SetCheck(check);
+
+    check = m_homeSettings.bLine1Enabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableLine1CheckBox.SetCheck(check);
+
+    check = m_homeSettings.bLine2Enabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableLine2CheckBox.SetCheck(check);
+
+    check = m_homeSettings.bLine2Enabled ? BST_CHECKED : BST_UNCHECKED;
+    m_enableLine3CheckBox.SetCheck(check);
+
+    return FALSE;
+}
+
+BEGIN_MESSAGE_MAP(CManilla2DConfigHomeDlg, CPropertyPage)
+    ON_WM_PAINT()
+    ON_MESSAGE(PSM_QUERYSIBLINGS, CManilla2DConfigHomeDlg::OnQuerySiblings)
+END_MESSAGE_MAP()
+
+void CManilla2DConfigHomeDlg::OnPaint()
+{
+    CPaintDC dc(this);
+
+    int nWidth = dc.GetDeviceCaps(HORZRES);
+    const int nHeaderHeight = 24;
+
+    // paint title
+    CFont *pCurrentFont = dc.GetCurrentFont();
+    LOGFONT lf;
+    pCurrentFont->GetLogFont(&lf);
+    lf.lfWeight = FW_BOLD;
+
+    CFont newFont;
+    newFont.CreateFontIndirect(&lf);
+    CFont *pSave = dc.SelectObject(&newFont);
+    dc.SetTextColor(RGB(0, 0, 156));
+    dc.DrawText(TEXT("Home Tab Settings"), CRect(8, 0, nWidth, nHeaderHeight), DT_VCENTER | DT_SINGLELINE); dc.SelectObject(pSave);
+
+    // paint line
+    CPen blackPen(PS_SOLID, 1, RGB(0,0,0));
+    CPen *pOldPen = dc.SelectObject(&blackPen);
+
+    dc.MoveTo(0, nHeaderHeight);
+    dc.LineTo(nWidth, nHeaderHeight);
+
+    dc.SelectObject(pOldPen); 
+}
+
+LRESULT CManilla2DConfigHomeDlg::OnQuerySiblings(WPARAM wParam, LPARAM lParam)
+{
+    return 0;
+}
+
+void CManilla2DConfigHomeDlg::OnOK()
+{
+    HomeWidgetSettings newSettings;
+
+    newSettings.bAnalogClockEnabled = (m_enableAnalogClockCheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bDigitalClockEnabled = (m_enableDigitalClockCheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bMissedCallsEnabled = (m_enableMissedCallsCheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bCalendarEnabled = (m_enableCalendarCheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bAlarmStatusEnabled = (m_enableAlarmStatusCheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bLine1Enabled = (m_enableLine1CheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bLine2Enabled = (m_enableLine2CheckBox.GetCheck() == BST_CHECKED);
+    newSettings.bLine3Enabled = (m_enableLine3CheckBox.GetCheck() == BST_CHECKED);
+
+    if((newSettings.bAnalogClockEnabled != m_homeSettings.bAnalogClockEnabled) ||
+        (newSettings.bDigitalClockEnabled != m_homeSettings.bDigitalClockEnabled) ||
+        (newSettings.bMissedCallsEnabled != m_homeSettings.bMissedCallsEnabled) ||
+        (newSettings.bCalendarEnabled != m_homeSettings.bCalendarEnabled) ||
+        (newSettings.bAlarmStatusEnabled != m_homeSettings.bAlarmStatusEnabled) ||
+        (newSettings.bLine1Enabled != m_homeSettings.bLine1Enabled) ||
+        (newSettings.bLine2Enabled != m_homeSettings.bLine2Enabled) ||
+        (newSettings.bLine3Enabled != m_homeSettings.bLine3Enabled))
+    {
+        M2DC::SetHomeWidgetSettings(M2DC::GetPathToHTCHomeSettingsXmlFileWorking(), &newSettings);
+    }
+}
--- Manilla2DConfig/Manilla2DConfig/README-THEMES.txt
+++ Manilla2DConfig/Manilla2DConfig/README-THEMES.txt
@@ -2,26 +2,46 @@ M2DC Theme Readme
 
 Theme management support was introduced in M2DC Version 0.6
 
-M2DC accepts theme files that are named *.m2dct
+So far each theme manager has supported themes in a different way the notes below are relavant for 0.9
 
-This file is a zip file that has been renamed from *.zip to *.m2dct
+M2DC THEME MANAGEMENT NOTES
+* M2DC Process
+    When a user clicks "Apply Theme" M2DC will first build a list of all of the files that M2CD is using
+        on the device.  This list is made up by reading the values out of the xml file and then reading the
+        rest of the "HH_*" files from the windows direcotry.
+        After the list is built M2DC starts to go though the zip file.  It looks at each file in the zip sep
 
-All a theme file is is a zip file with the a HTCHomeSettings.xml file, and all of the HH_ files
-than Manilla 2D uses to display graphics.
+* HTCHomeSettings.xml
+    If this file exists in your theme then M2DC will read values from the main <WidgetProperty> node.
+        It will only read values that have the string "POS" or "RECT" in their name attribute.  I only
+        read these values because I don't know of a situation where a theme should change other values
+        (maybe width/height values???).  Please let me know if you change values other than POS and RECT
+        in your theme.
+     
+* Hiding Clocks
+    A common feature of a skin is to hide the digital or analog clock fomr the home widget.  I have seen
+        more than a handful of different ways to accomplish this and no one seems ot agree on a standard.
+        I am putting in writing my opinion and the way that M2DC hides clocks.  When I want to hide something
+        I just set the POS and RECT values to "-1,-1" and "-1,-1,-1,-1" respectively.  This hides the item
+        and has no other adverse side effects on the users htchomesettings.xml file.
+     
+THEME FILE DETAILS
+* Your theme file must either have the extension of .zip of .m2dct.
+* All that you need to do is zip up your theme files with an optional HTCHomeSettings.xml file.
+* Zip File Contents
+    * If your zip file does not conatin an HTCHomeSettings.xml file or a file that matching the
+        search string "hh_*", then M2DC will not allow the user to use your theme.
+    * If you do not include an HTCHomeSettings.xml file then the users backup HTCHomeSettings.xml
+        file will be used for image positions and other theme read values.
+* Future
+    In the future other files may be supported.
+    * One example is a *.tsk file, maybe I can figure out how to programmatically apply the tsk
+        theme when the suer applies the M2DC theme.
+    * Another example would be a screenshot.jpg or soemthing liek that.  I t would allow for users
+        to get a general dea of what the theme looks like from within the theme manager.
+     
+NOTES
+ * Currently No skins are distributed with M2DC, if you would like to contribute a skin 
+    for me to distribute please contact me and we will see what we can work out.
 
-When a user asks for a specific theme to be applied the customizer will
-1.) Open the m2dct file.
-2.) Extract all the files in it to %InstalDir%\ActiveTheme
-     (ignoring the directory structure of the zip file)
-3.) Read specific values from the HTCHomeSettings.xml file and write them to the actual 
-     HTCHomeSettings.xml file locate din \Windows.
-     - Currently the customize will read the following values
-	<HTCHome><ImageList> elements with ids 121-128, and 209-216
-	<HTCHome><WidgetProperty><HomeWidget> elements  with indexes 0-10
-
-CUrrently No skins are distributed with M2DC, if you would like to contribute a skin 
-for me to distribute please contact me and we will see what we can work out.
-
-This readme may get more detailed to answer FAQs.
-
-I reserve the right to change the file format, but I highly doubt I would ever do that.
+ * I reserve the right to change the file format, but I highly doubt I would ever do that.
