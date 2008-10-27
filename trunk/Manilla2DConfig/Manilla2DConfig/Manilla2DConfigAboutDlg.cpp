710ec416132b174a5302649856f5161f7365ca0f
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-10-27 22:16:25 +0000

    updated donator list

--- trunk/Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
+++ trunk/Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
@@ -71,6 +71,7 @@ BOOL CManilla2DConfigAboutDlg::OnInitDialog()
     m_donatorsListBox.AddString(TEXT("showaco - $$$, Beta Tester v14, $$$"));
     m_donatorsListBox.AddString(TEXT("debarron - Beta Tester v14"));
     m_donatorsListBox.AddString(TEXT("dashmaster1000 - Beta Tester v14"));
+    m_donatorsListBox.AddString(TEXT("ldostang - $$$"));
     m_donatorsListBox.AddString(TEXT("..."));
     m_donatorsListBox.AddString(TEXT("<your name here for a donation of $1 or more>"));
 
