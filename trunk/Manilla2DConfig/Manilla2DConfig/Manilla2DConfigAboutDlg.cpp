858393bf9b81941ef9ae04a872b0933b33686450
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-11-17 15:52:21 +0000

    updated donator list

--- trunk/Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
+++ trunk/Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
@@ -78,6 +78,8 @@ BOOL CManilla2DConfigAboutDlg::OnInitDialog()
     m_donatorsListBox.AddString(TEXT("sschlesinger - $$$"));
     m_donatorsListBox.AddString(TEXT("Zak from ZaksEnterprises.com - $$$"));
     m_donatorsListBox.AddString(TEXT("scotchua - $$$"));
+    m_donatorsListBox.AddString(TEXT("jesusishere - $$$"));
+    m_donatorsListBox.AddString(TEXT("Jaxboater - $$$"));
     m_donatorsListBox.AddString(TEXT("..."));
     m_donatorsListBox.AddString(TEXT("<your name here for a donation of $1 or more>"));
 
