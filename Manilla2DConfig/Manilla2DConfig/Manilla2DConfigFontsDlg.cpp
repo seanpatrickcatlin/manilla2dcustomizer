8f9944a2b5ba7a30b67fbd4b919056777cb48572
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-11-22 05:27:47 +0000

    fonts drop down boxes are much larger now, much more easy to read

--- Manilla2DConfig/Manilla2DConfig/Manilla2DConfigFontsDlg.cpp
+++ Manilla2DConfig/Manilla2DConfig/Manilla2DConfigFontsDlg.cpp
@@ -46,6 +46,22 @@ CManilla2DConfigFontsDlg::~CManilla2DConfigFontsDlg()
 {
 }
 
+// all credits for this method go to 
+// http://dotnetbutchering.blogspot.com/2008/03/vc-mfc-how-to-set-combobox-dropdown.html
+void SetDropDownHeight(CComboBox* pMyComboBox, int itemsToShow)
+{
+  //Get rectangles
+  CRect rctComboBox, rctDropDown;
+  pMyComboBox->GetClientRect(&rctComboBox); //Combo rect
+  pMyComboBox->GetDroppedControlRect(&rctDropDown); //DropDownList rect
+
+  int itemHeight = pMyComboBox->GetItemHeight(-1); //Get Item height
+
+  pMyComboBox->GetParent()->ScreenToClient(&rctDropDown); //Converts coordinates
+  rctDropDown.bottom = rctDropDown.top + rctComboBox.Height() + itemHeight*itemsToShow; //Set height
+  pMyComboBox->MoveWindow(&rctDropDown); //enable changes
+}
+
 void CManilla2DConfigFontsDlg::DoDataExchange(CDataExchange* pDX)
 {
     CPropertyPage::DoDataExchange(pDX);
