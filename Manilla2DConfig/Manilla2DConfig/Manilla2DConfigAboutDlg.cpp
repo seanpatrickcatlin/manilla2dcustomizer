2c0dd074d35c5b6a2da00cc0862167080bf499b5
catlin.sean <catlin.sean@6f84e72e-853c-11dd-88bd-cff828a63736>
2008-11-24 04:39:59 +0000

    updated build number to 0.23.223

--- Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
+++ Manilla2DConfig/Manilla2DConfig/Manilla2DConfigAboutDlg.cpp
@@ -81,6 +81,7 @@ BOOL CManilla2DConfigAboutDlg::OnInitDialog()
     m_donatorsListBox.AddString(TEXT("jesusishere - $$$"));
     m_donatorsListBox.AddString(TEXT("Jaxboater - $$$"));
     m_donatorsListBox.AddString(TEXT("RaNg3r - $$$"));
+    m_donatorsListBox.AddString(TEXT("scrosler - $$$"));
     m_donatorsListBox.AddString(TEXT("..."));
     m_donatorsListBox.AddString(TEXT("<your name here for a donation of $1 or more>"));
 
